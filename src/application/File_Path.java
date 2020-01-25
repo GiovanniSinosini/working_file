0d09943b1b08aa3e0b4935e7a8acc475d7625930
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-01-25 02:12:36 +0000

    File path

---
+++ src/application/File_Path.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class File_Path {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a file path: ");
+		String strPath = sc.nextLine();
+		
+		
+		File path = new File(strPath);
+		
+		System.out.println("getName: " + path.getName());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getPath: " + path.getPath());
+		
+		
+		sc.close();
+		
+		
+		
+		
+	}
+
+}
